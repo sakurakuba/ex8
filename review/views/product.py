d1805ec182db2e1160818aeb0ce5ff8b6b162206
Kuba <ksulaymanov@gmail.com>
2022-08-27 07:55:06 -0500

    added permissions

--- review/views/product.py
+++ review/views/product.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -20,28 +21,33 @@ class ProductView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['reviews'] = self.object.products.order_by("-created_at")
+        u = self.request.user.groups.filter(name__in=['Moderators']).exists()
+        context['u'] = u
         return context
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(PermissionRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'product/product_create.html'
+    permission_required = "review.add_product"
 
     def get_success_url(self):
         return reverse('review:product_view', kwargs={'pk': self.object.pk})
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(PermissionRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'product/product_update.html'
+    permission_required = "review.change_product"
 
     def get_success_url(self):
         return reverse('review:product_view', kwargs={'pk': self.object.pk})
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(PermissionRequiredMixin, DeleteView):
     model = Product
     template_name = 'product/product_delete.html'
     success_url = reverse_lazy('review:index')
+    permission_required = "review.delete_product"
--- review/views/review.py
+++ review/views/review.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import CreateView, UpdateView, DeleteView
@@ -6,7 +7,7 @@ from review.forms import ReviewForm
 from review.models import Product, Review
 
 
-class CreateReview(CreateView):
+class CreateReview(LoginRequiredMixin, CreateView):
     form_class = ReviewForm
     template_name = "review/review_create.html"
 
@@ -21,20 +22,28 @@ class CreateReview(CreateView):
         return reverse("review:product_view", kwargs={"pk": self.object.product.pk})
 
 
-class UpdateReview(UpdateView):
+class UpdateReview(PermissionRequiredMixin, UpdateView):
     form_class = ReviewForm
     template_name = "review/review_update.html"
     model = Review
+    permission_required = "review.change_review"
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
 
     def get_success_url(self):
         return reverse("review:product_view", kwargs={"pk": self.object.product.pk})
 
 
-class DeleteReview(DeleteView):
+class DeleteReview(PermissionRequiredMixin, DeleteView):
     model = Review
+    permission_required = "review.delete_review"
 
     def get(self, request, *args, **kwargs):
         return super().delete(request, *args, **kwargs)
+
+    def has_permission(self):
+        return super().has_permission() or self.request.user == self.get_object().author
 
     def get_success_url(self):
         return reverse("review:product_view", kwargs={"pk": self.object.product.pk})
